1b19ec19aa0ada1200b4548f62dbb64bb8891fee
Szymon Nowicki <sznowicki@eskalacja.com>
2023-12-06 19:05:27 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Minimal eslint config that enforces tabs
+
+There's not much more. Just bare minimum, tabs, single quotes. That's it.
---
+++ eslintconfig.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    'rules': {
+        'indent': [
+            'error',
+            'tab'
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
